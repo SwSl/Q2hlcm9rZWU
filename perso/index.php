9c179f8b9c847e75ebc066e4a6a2c24348dfa674
SwSl <antoine.girardin@epitech.eu>
2018-10-12 00:04:10 +0200

    Create index.php

---
+++ perso/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Fake Epitech Perso Portal</title>
+</head>
+<body>
+	<p>Welcome to the new private portal!</p>
+</body>
+</html>
