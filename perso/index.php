a025be58a6e6416fc72c6f8debb2fce323d216c0
SwSl <antoine.girardin@epitech.eu>
2018-10-19 08:40:15 +0200

    Update index.php

--- perso/index.php
+++ perso/index.php
@@ -1,3 +1,20 @@
+<?php
+$c = array(
+	'root',
+	'');
+$db = new PDO('mysql:host=localhost;dbname=perso', $c[0], $c[1]);
+if (!isset($_SESSION["isLogged"])) {
+	$user = mysql_real_escape_string($_POST["user"] ?? "");
+	$pass = mysql_real_escape_string($_POST["pass"] ?? "");
+	if ($user != "" && $pass != "") {
+		$res = $db->query("SELECT 1 from users where user == '$user' and pass == '$pass'");
+		if (isset($res->fetch()))
+			$_SESSION["isLogged"] = true;
+	}
+} else {
+	// process update
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +22,22 @@
 	<title>Fake Epitech Perso Portal</title>
 </head>
 <body>
+	<?php
+	if (isset($_SESSION["isLogged"])) {
+	?>
 	<p>Welcome to the new private portal!</p>
+	<?php
+	} else {
+	?>
+	<form method="POST">
+		<label for="user">User:</label>
+		<input type="text" name="user" id="user" /><br />
+		<label for="pass">Password:</label>
+		<input type="password" name="pass" id="pass" /><br />
+		<input type="submit" value="Log In" />
+	</form>
+	<?php
+	}
+	?>
 </body>
 </html>
